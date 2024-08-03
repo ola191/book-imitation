450d0b8dd5b5c93b9976f3271b571b293e7a5cac
Ola Krassowska <ola.krassowska@hotmail.com>
2024-08-04 00:09:21 +0200

    Added custom tag and animating text

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("loaded!");
-});
+class WriteLikeRealText extends HTMLElement {
+    connectedCallback() {
+        let text = this.textContent;
+        this.textContent = '';
+        this.animateText(text);
+    }
+
+    animateText(text) {
+        let index = 0;
+        const interval = setInterval(() => {
+            this.textContent += text[index];
+            index++;
+            if (index === text.length) {
+                clearInterval(interval);
+            }
+        }, 100);
+    }
+}
+
+customElements.define('writereal-text', WriteLikeRealText);
